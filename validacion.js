7db5a0242476925074b9b5207fa1dd48e20ddd88
Ivan Escalante <licovacterpilori20@gmail.com>
2023-01-15 22:04:55 -0500

    Agregando otra funcion para el mismo evento de la imagen

--- validacion.js
+++ validacion.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     var nirvanaLogo = ".nirvana_logo";
+    var nirvanaBand = ".nirvana_band";
 
     $(nirvanaLogo).click(function(){
         $('.flip_box').toggleClass('flipped');
         return false;
 
     });
+
+    $(nirvanaBand).click(function(){
+        $('.flip_box').toggleClass('flipped');
+        return false;
+
+    });
+
 
 });
